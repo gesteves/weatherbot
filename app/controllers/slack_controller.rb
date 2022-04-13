cd0a4577156bd45e5aa6fc7e82e966b450ba9ce2
Guillermo Esteves <g@gesteves.com>
2022-04-13 08:07:40 -0600

    Copy slack lib

---
+++ app/controllers/slack_controller.rb
@@ -0,0 +1,115 @@
+class SlackController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :no_cache
+  before_action :parse_event, only: :events
+  before_action :parse_interaction, only: :interactions
+  before_action :parse_slash, only: :slash
+  before_action :check_token, only: [:events, :interactions, :slash]
+  before_action :verify_url, only: :events
+
+  def auth
+    url = root_url
+    if params[:code].present?
+      slack = Slack.new
+      token = slack.get_access_token(code: params[:code], redirect_uri: auth_url)
+      if token[:ok]
+        access_token = token[:access_token]
+        team_id = token.dig(:team, :id)
+        team = Team.find_or_create_by(slack_id: team_id)
+        team.access_token = access_token
+        if team.save
+          notice = nil
+          url = success_url
+        else
+          notice = 'Oh no, something went wrong. Please try again!'
+        end
+      else
+        logger.error "[LOG] Authentication failed for the following reason: #{token[:error]}"
+        notice = "Oh no, something went wrong. Please try again!"
+      end
+    elsif params[:error].present?
+      logger.error "[LOG] Authentication failed for the following reason: #{params[:error]}"
+      notice = "Trebekbot was not added to your Slack. Please try again!"
+    end
+    redirect_to url, notice: notice, allow_other_host: true
+  end
+
+  def events
+    case @event_type
+    when 'app_home_opened'
+      app_home_opened
+    when 'app_uninstalled'
+      app_uninstalled
+    end
+
+    render plain: "OK", status: 200
+  end
+
+  def interactions
+    render plain: "OK", status: 200
+  end
+
+  def slash
+    render plain: "OK", status: 200
+  end
+
+  private
+
+  def check_token
+    render plain: "Unauthorized", status: 401 if @token != ENV['SLACK_VERIFICATION_TOKEN']
+  end
+
+  def parse_event
+    @token = params[:token]
+    @event_type = params.dig(:event, :type) || params[:type]
+    @text = params.dig(:event, :text)
+    @team = params[:team_id]
+    @channel = params.dig(:event, :channel)
+    @user = params.dig(:event, :user)
+    @thread_ts = params.dig(:event, :thread_ts)
+  end
+
+  def parse_interaction
+    begin
+      payload = JSON.parse(params[:payload], symbolize_names: true)
+    rescue
+      return render plain: "Bad Request", status: 400
+    end
+
+    @token = payload[:token]
+    @user = payload.dig(:user, :id)
+    @team = payload.dig(:team, :id)
+    @channel = payload.dig(:channel, :id)
+    @ts = payload.dig(:message, :ts)
+    @answer = payload.dig(:actions)&.find { |a| a[:action_id] == "answer" }.dig(:value)
+  end
+
+  def parse_slash
+    @token = params[:token]
+    @team = params[:team_id]
+    @channel = params[:channel_id]
+    @user = params[:user_id]
+    @command = params[:command]
+    @text = params[:text]
+  end
+
+  # EVENT HANDLERS
+
+  def verify_url
+    render plain: params[:challenge], status: 200 if @event_type == 'url_verification'
+  end
+
+  def app_home_opened
+    #UpdateAppHomeWorker.perform_async(@team, @user)
+  end
+
+
+  def app_uninstalled
+    team = Team.find_by(slack_id: @team)
+    team.destroy
+  end
+
+  # INTERACTION HANDLERS
+
+  # SLASH HANDLERS
+end
