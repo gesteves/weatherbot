8644ab683d83665cc30fd58caae1b14f13592813
Guillermo Esteves <g@gesteves.com>
2022-04-15 11:47:46 -0600

    Fix

--- app/presenters/forecast_presenter.rb
+++ app/presenters/forecast_presenter.rb
@@ -41,10 +41,10 @@ class ForecastPresenter < SimpleDelegator
     blocks << precipitation_line_chart(data: dig(:minutely, :data), time_format: '%l:%M %P', ticks: 28)
     blocks << divider
     blocks << hourly_block
-    blocks << precipitation_temperature_line_chart(data: dig(:hourly, :data)&.select { |d| d[:time] > now }&.slice(0, 24), time_format: '%l %P')
+    blocks << precipitation_temperature_line_chart(data: dig(:hourly, :data)&.select { |d| d[:time] > Time.now }&.slice(0, 24), time_format: '%l %P')
     blocks << divider
     blocks << daily_block
-    blocks << precipitation_temperature_combo_chart(data: dig(:daily, :data)&.select { |d| d[:time] > now }&.slice(0, 7), time_format: '%A')
+    blocks << precipitation_temperature_combo_chart(data: dig(:daily, :data)&.select { |d| d[:time] > Time.now }&.slice(0, 7), time_format: '%A')
     blocks << divider
     blocks.flatten.compact
   end
