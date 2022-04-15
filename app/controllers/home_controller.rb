29c0173cdfb5dd8501d30952802da9e17034c107
Guillermo Esteves <g@gesteves.com>
2022-04-15 10:49:32 -0600

    Add screenshot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   before_action :set_max_age
 
   def index
+    @teams_count = Team.all.count
+    @users_count = User.all.count
     @noindex = false
   end
 
